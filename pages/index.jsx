c778a8496d7d6ab6a2f70f3ce080862aaed7a1b0
Barak Korren <bkorren@redhat.com>
2023-10-10 11:14:57 +0300

    Display item state, notes and url
    * State displayed via separate columns for separate states.
    * URL displayed by making the title a link
    * Notes are displayed as text beneath the title.
    
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,10 +6,38 @@ export async function getStaticProps() {
 
 export default function Page({ data }) {
     console.log(data)
-    return <>
+    const columns = ["To Read", "Reading", "Read"]
+    return <main>
+        <style jsx>{`
+            .columns {
+                display: grid;
+                grid-template-columns: repeat(3, 1fr);
+            }
+        `}</style>
         <h1>My reading list</h1>
-        <ul>
-            {data.map((item, idx) => <li key={idx}>{item.title}</li>)}
-        </ul>
-    </>
+        <div className="columns">
+            {columns.map((col) => <Column
+                key={col}
+                title={col}
+                items={data.filter(({status}) => status.toLowerCase() === col.toLowerCase())}
+            />)}
+        </div>
+    </main>
+}
+
+export function Column({ title, items }) {
+    return <section className="root">
+        <style jsx>{`
+            .root { padding: 1em }
+        `}</style>
+        <h2>{title}</h2>
+        {items.map((item) => <Item key={item.key} {...item} />)}
+    </section>
+}
+
+export function Item({ title, url, notes }) {
+    return <article>
+        <h3><a href={url}>{title}</a></h3>
+        <p>{notes}</p>
+    </article>
 }
